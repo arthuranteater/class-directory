339946479ba08819befdc635dd0fab7778dc628f
Hunt Applegate <huntapplegate@gmail.com>
2018-09-14 10:24:18 -0600

    added knex pg

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ const express = require("express");
 const app = express();
 const parser = require("body-parser");
 const cors = require("cors");
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("😎");
+});
+
+app.listen(port, () => {
+  console.log("it works");
+});
